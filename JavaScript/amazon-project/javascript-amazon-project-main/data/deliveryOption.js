609d670b8f2b8c6ee8be6f4f4d4c6ebe946ad4f7
JINLING1 <1124290657@qq.com>
2025-06-13 19:02:33 +0800

    save data and generate the HTML for delivery options

---
+++ JavaScript/amazon-project/javascript-amazon-project-main/data/deliveryOption.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+    id: '1',
+    delivertDays:7,
+    priceCents:0
+},{
+    id:'2',
+    delivertDays:3,
+    priceCents:499 //use cents to avoid floating point errors
+},{
+    id:'3',
+    delivertDays:1,
+    priceCents:999
+}];
