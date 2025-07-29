a449e030dbb7d3b86ff2e0a541438c4b31d97b2b
JINLING1 <1124290657@qq.com>
2025-07-29 10:47:08 +0800

    Create automated tests for addToCart

--- JavaScript/amazon-project/javascript-amazon-project-main/data/cart.js
+++ JavaScript/amazon-project/javascript-amazon-project-main/data/cart.js
@@ -1,18 +1,24 @@
 //购物车数据
-export let cart = JSON.parse(localStorage.getItem("cart"));
-if (!cart) {//if cart is null 
-  cart = [
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId:'1'
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId:'2'
-    }
-  ];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
+  }
 }
 function saveToStorage() {
   localStorage.setItem("cart", JSON.stringify(cart));
@@ -30,7 +36,7 @@ export function addToCart(productId) {
     cart.push({
       productId,
       quantity: 1,
-      deliveryOptionId:'1'
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
@@ -50,7 +56,7 @@ export function removeFromCart(productId) {
   saveToStorage();
 }
 //update delivery option
-export function updateDeliveryOption(productId,deliveryOptionId){
+export function updateDeliveryOption(productId, deliveryOptionId) {
   //find the item in the cart with the matching productId
   let matchingItem;
   cart.forEach((cartItem) => {
@@ -59,6 +65,6 @@ export function updateDeliveryOption(productId,deliveryOptionId){
     }
   });
   matchingItem.deliveryOptionId = deliveryOptionId;
-  
+
   saveToStorage();
 }
