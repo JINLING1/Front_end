81c9988ff11ad7d78160120dbeabcf6997c98ea7
JINLING1 <1124290657@qq.com>
2025-09-08 17:47:21 +0800

    Load products from the backend

--- JavaScript/amazon-project/javascript-amazon-project-main/data/products.js
+++ JavaScript/amazon-project/javascript-amazon-project-main/data/products.js
@@ -1,60 +1,59 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from '../scripts/utils/money.js';
 
-export function getProduct(productId){
-  let matchingProduct;
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+export function getProduct(productId) {
+	let matchingProduct;
+	products.forEach((product) => {
+		if (product.id === productId) {
+			matchingProduct = product;
+		}
+	});
 
-    return matchingProduct;
+	return matchingProduct;
 }
 
 class Product {
-  id;
-  image;
-  name;
-  rating;
-  priceCents;
+	id;
+	image;
+	name;
+	rating;
+	priceCents;
 
-  constructor(productDetails){
-    this.id = productDetails.id;
-    this.image = productDetails.image;
-    this.name = productDetails.name;
-    this.rating = productDetails.rating;
-    this.priceCents = productDetails.priceCents;
-  }
+	constructor(productDetails) {
+		this.id = productDetails.id;
+		this.image = productDetails.image;
+		this.name = productDetails.name;
+		this.rating = productDetails.rating;
+		this.priceCents = productDetails.priceCents;
+	}
 
-  getStarsUrl(){
-    return `images/ratings/rating-${this.rating.stars * 10}.png`;
-  }
+	getStarsUrl() {
+		return `images/ratings/rating-${this.rating.stars * 10}.png`;
+	}
 
-  getPrice(){
-    return `$${formatCurrency(this.priceCents)}`
-  }
+	getPrice() {
+		return `$${formatCurrency(this.priceCents)}`;
+	}
 
-  extraInfoHTML() {
-    return '';
-  }
-  
+	extraInfoHTML() {
+		return '';
+	}
 }
 
-class Clothing extends Product  {
-  sizeChartLink;
+class Clothing extends Product {
+	sizeChartLink;
 
-  constructor(productDetails){
-    super(productDetails);
-    this.sizeChartLink = productDetails.sizeChartLink;
-  }
+	constructor(productDetails) {
+		super(productDetails);
+		this.sizeChartLink = productDetails.sizeChartLink;
+	}
 
-  extraInfoHTML() {
-    return `
+	extraInfoHTML() {
+		return `
       <a href="${this.sizeChartLink}" target="_blank">
        Size chart
       </a>
     `;
-  }
+	}
 }
 
 /*
@@ -62,7 +61,6 @@ const date = new Date();
 console.log(date);
 console.log(date.toLocaleTimeString());
 */
-
 
 /*
 function logThis(){
@@ -88,6 +86,29 @@ const obj2 = {
 obj2.print();
 */
 
+export let products = [];
+
+export function loadProducts(fun) {
+	const xhr = new XMLHttpRequest();
+
+	xhr.addEventListener('load', () => {
+		products = JSON.parse(xhr.response).map((productDetails) => {
+			if (productDetails.type === 'clothing') {
+				return new Clothing(productDetails);
+			}
+			return new Product(productDetails);
+		});
+
+		console.log('load products');
+
+    fun();
+	});
+
+	xhr.open('GET', 'https://supersimplebackend.dev/products');
+	xhr.send();
+}
+loadProducts();
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -753,3 +774,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
