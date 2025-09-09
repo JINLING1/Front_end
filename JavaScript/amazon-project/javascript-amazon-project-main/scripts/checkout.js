7132f4ece0655cc0176b357784395e1aa4523071
JINLING1 <1124290657@qq.com>
2025-09-09 15:47:51 +0800

    Learn promises and convert code to promises

--- JavaScript/amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ JavaScript/amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -1,11 +1,51 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
-import {loadProducts} from '../data/products.js';
+import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('value1'); 
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then((values)=>{
   renderOrderSummary();
   renderPaymentSummary();
 });
 
+/*
+new Promise((resolve) => {
+	loadProducts(() => {
+		resolve('value1'); //resolve when loadProducts is done
+	});
+
+}).then((value) => { //接收resolve传过来的值
+		return new Promise((resolve) => {
+			loadCart(() => {
+				resolve();
+			});
+		});
+
+	}).then(() => {
+		renderOrderSummary();
+		renderPaymentSummary();
+	});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
